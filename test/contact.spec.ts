67c85c125be4336169abdb10a18d15c288109392
nopaldonh <nopaldonh21@gmail.com>
2025-05-28 14:41:21 +0700

    update contact api

--- test/contact.spec.ts
+++ test/contact.spec.ts
@@ -119,4 +119,78 @@ describe('ContactController', () => {
       expect(resBody.data?.phone).toBe('9999');
     });
   });
+
+  describe('PUT /api/contacts/:contactId', () => {
+    beforeEach(async () => {
+      await testService.deleteContact();
+      await testService.deleteUser();
+
+      await testService.createUser();
+      await testService.createContact();
+    });
+
+    it('should be rejected if request is invalid', async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .put(`/api/contacts/${(contact as Contact).id}`)
+        .set('Authorization', 'test')
+        .send({
+          first_name: '',
+          last_name: '',
+          email: 'salah',
+          phone: '',
+        });
+
+      const resBody = response.body as WebResponse<ContactResponse>;
+
+      logger.info(resBody);
+
+      expect(response.status).toBe(400);
+      expect(resBody.errors).toBeDefined();
+    });
+
+    it('should be rejected if contact is not found', async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .put(`/api/contacts/${(contact as Contact).id + 1}`)
+        .set('Authorization', 'test')
+        .send({
+          first_name: 'test',
+          last_name: 'test',
+          email: 'test@example.com',
+          phone: '9999',
+        });
+
+      const resBody = response.body as WebResponse<ContactResponse>;
+
+      logger.info(resBody);
+
+      expect(response.status).toBe(404);
+      expect(resBody.errors).toBeDefined();
+    });
+
+    it('should be able to update contact', async () => {
+      const contact = await testService.getContact();
+      const response = await request(app.getHttpServer())
+        .put(`/api/contacts/${(contact as Contact).id}`)
+        .set('Authorization', 'test')
+        .send({
+          first_name: 'test updated',
+          last_name: 'test updated',
+          email: 'testupdated@example.com',
+          phone: '8888',
+        });
+
+      const resBody = response.body as WebResponse<ContactResponse>;
+
+      logger.info(resBody);
+
+      expect(response.status).toBe(200);
+      expect(resBody.data?.id).toBeDefined();
+      expect(resBody.data?.first_name).toBe('test updated');
+      expect(resBody.data?.last_name).toBe('test updated');
+      expect(resBody.data?.email).toBe('testupdated@example.com');
+      expect(resBody.data?.phone).toBe('8888');
+    });
+  });
 });
